2c5b4777dd3b61551b765f0773494e9e0073f35e
Matt Harriger <mharriger@gmail.com>
2026-06-16 21:32:43 -0500

    Non-frame changes

--- src/kayakulator.py
+++ src/kayakulator.py
@@ -5,8 +5,10 @@ Sets up the GUI, and handles the modeling and output if run with --no-gui flag
 
 from gui.mainwindow import MainWindow
 from PySide6.QtWidgets import QApplication
+import settings_manager
 
 if __name__ == "__main__":
+    settings_manager.settings = settings_manager.SettingsManager() # Set this to an instance of a different subclass of SettingsManager to manage settings differently
     app = QApplication()
 
     window = MainWindow()
---
+++ src/settings_manager.py
@@ -0,0 +1,22 @@
+from offsets.member import MemberType
+
+settings = None
+
+class SettingsManager:
+    """Provides default settings. Can subclass to provide user setting storage."""
+    def __init__(self):       
+        # Define your default settings here for easy expansion later
+        self.DEFAULTS = {
+            (MemberType.FRAME, "frame_thickness"): 0.5 * 25.4,
+            (MemberType.FRAME, "frame_width"): 1 * 25.4,
+            (MemberType.FRAME, "skin_relief_depth"): 0.1,
+            (MemberType.FRAME, "interior_fillet_radius"): 0.5 * 25.4,
+        }
+
+    def get(self, key):
+        """Retrieve a setting. Falls back to default if not set by the user."""
+        return self.DEFAULTS.get(key, None)
+
+    def set(self, key, value):
+        """Save a user preference."""
+        pass
